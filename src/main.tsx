1ae80cf04cd531f7b77ecd61d7316faeafcaaef3
wenke.lai <wenke.studio@gmail.com>
2024-06-09 15:01:44 +0800

    build: do not use relative paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "src/App.tsx";
 
-import "./index.css";
+import "src/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
