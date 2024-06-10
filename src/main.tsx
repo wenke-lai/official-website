822e65fcb33b11ae2938798cc9840aa45e27d720
wenke.lai <wenke.studio@gmail.com>
2024-06-10 21:07:50 +0800

    feat: import Nunito font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "src/App.tsx";
 import ThemeProvider from "src/contexts/theme-provider";
 
+// Supports weights 200-900
+import "@fontsource-variable/nunito";
 import "src/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
