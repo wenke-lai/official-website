f48014744c558a619413ae2fd4e5dba1aca11584
wenke.lai <wenke.studio@gmail.com>
2024-07-06 00:16:47 +0800

    fix: set the log level to info and disable telemetry

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,10 @@
 import reflex as rx
+from reflex.constants import LogLevel
 
 config = rx.Config(
     app_name="official_website",
+    # Set the log level (Default is LogLevel.INFO.)
+    loglevel=LogLevel.DEBUG,
+    # Disable telemetry (Opt-out of Reflex's telemetry system. Default is True.)
+    telemetry_enabled=False,
 )
